d57956b46263c5f92545eabec0ae361c0466163b
Gregor Lichtner <gregor.lichtner@charite.de>
2022-11-02 17:22:57 +0100

    reading all recommendations

--- main.py
+++ main.py
@@ -4,11 +4,24 @@ os.environ["FHIR_BASE_URL"] = "http://localhost:8000/fhir"
 os.environ["OMOP_WEBAPI_URL"] = "http://192.168.200.128:9876/WebAPI"
 os.environ["FHIR_TERMINOLOGY_SERVER_URL"] = "http://tx.fhir.org/R4"
 
-from execution_engine.execution_engine import ExecutionEngine
+from execution_engine import ExecutionEngine
 
-# recommendation_url = "https://www.netzwerk-universitaetsmedizin.de/fhir/codex-celida/recommendations/intervention-plan/antithrombotic-prophylaxis-LMWH"
-recommendation_url = "https://www.netzwerk-universitaetsmedizin.de/fhir/codex-celida/recommendations/intervention-plan/therapeutic-anticoagulation"
+base_url = (
+    "https://www.netzwerk-universitaetsmedizin.de/fhir/codex-celida/recommendations/"
+)
+
+urls = [
+    "intervention-plan/antithrombotic-prophylaxis-LMWH",
+    "intervention-plan/therapeutic-anticoagulation",
+    "intervention-plan/antithrombotic-prophylaxis-fondaparinux",
+    "intervention-plan/no-antithrombotic-prophylaxis",
+    "intervention-plan/ventilation-plan",
+    "intervention-plan/peep-for-ards-fio2-point3",
+    "intervention-plan/abdominal-positioning-ARDS-plan",
+]
 
 e = ExecutionEngine()
 
-cd = e.process_recommendation(recommendation_url)
+for recommendation_url in urls:
+    print(recommendation_url)
+    cd = e.process_recommendation(base_url + recommendation_url)
