37c7668aeade0447de731c1ab35a859fbfd41c73
Gregor Lichtner <gregor.lichtner@charite.de>
2023-04-18 11:26:03 +0200

    added recommendation test run cli option

--- conftest.py
+++ conftest.py
@@ -17,6 +17,12 @@ def pytest_addoption(parser):  # type: ignore
     parser.addoption(
         "--run-slow-tests", action="store_true", default=False, help="run slow tests"
     )
+    parser.addoption(
+        "--run-recommendation-tests",
+        action="store_true",
+        default=False,
+        help="run recommendation tests",
+    )
 
 
 def pytest_configure(config):  # type: ignore
@@ -26,6 +32,9 @@ def pytest_configure(config):  # type: ignore
     See https://docs.pytest.org/en/latest/example/markers.html#marking-test-functions-and-selecting-them-for-a-run
     """
     config.addinivalue_line("markers", "slow: mark test as slow to run")
+    config.addinivalue_line(
+        "markers", "recommendation: mark test as a recommendation test"
+    )
 
 
 def pytest_collection_modifyitems(config, items):  # type: ignore
@@ -34,15 +43,23 @@ def pytest_collection_modifyitems(config, items):  # type: ignore
 
     See https://docs.pytest.org/en/latest/example/simple.html#control-skipping-of-tests-according-to-command-line-option
     """
-    if config.getoption("--run-slow-tests"):
-        # --run-slow given in cli: do not skip slow tests
-        return
+    if not config.getoption("--run-slow-tests"):
+        # --run-slow not given in cli: skip slow tests
+        skip_slow = pytest.mark.skip(reason="need --run-slow-tests option to run")
 
-    skip_slow = pytest.mark.skip(reason="need --run-slow-tests option to run")
+        for item in items:
+            if "slow" in item.keywords:
+                item.add_marker(skip_slow)
 
-    for item in items:
-        if "slow" in item.keywords:
-            item.add_marker(skip_slow)
+    if not config.getoption("--run-recommendation-tests"):
+        # --run-recommendation-tests not given in cli: skip recommendation tests
+        skip_recommendation = pytest.mark.skip(
+            reason="need --run-recommendation-tests option to run"
+        )
+
+        for item in items:
+            if "recommendation" in item.keywords:
+                item.add_marker(skip_recommendation)
 
 
 @pytest.fixture
@@ -51,3 +68,11 @@ def run_slow_tests(request) -> bool:  # type: ignore
     Fixture to determine if slow tests should be run.
     """
     return request.config.getoption("--run-slow-tests")
+
+
+@pytest.fixture
+def run_recommendation_tests(request) -> bool:  # type: ignore
+    """
+    Fixture to determine if recommendation tests should be run.
+    """
+    return request.config.getoption("--run-recommendation-tests")
