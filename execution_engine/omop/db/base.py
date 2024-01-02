c95e12b8d94906ba4ecb431024fe72cb178f057a
Gregor Lichtner <gregor.lichtner@med.uni-greifswald.de>
2024-01-02 15:17:04 +0100

    updated DateTime type

--- execution_engine/omop/db/base.py
+++ execution_engine/omop/db/base.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 import datetime
 
-from sqlalchemy import TIMESTAMP, DateTime
+from sqlalchemy import DateTime
 from sqlalchemy.orm import DeclarativeBase
 
 DateTimeWithTimeZone = DateTime(timezone=True)
@@ -9,7 +9,7 @@ DateTimeWithTimeZone = DateTime(timezone=True)
 
 class Base(DeclarativeBase):  # noqa: D101
     type_annotation_map = {
-        datetime.datetime: TIMESTAMP(timezone=True),
+        datetime.datetime: DateTimeWithTimeZone,
     }
 
 
