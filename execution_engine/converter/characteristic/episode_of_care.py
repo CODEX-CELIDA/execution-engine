720ca89a3b4941a3bdb2559fb63593471b702415
Gregor Lichtner <gregor.lichtner@med.uni-greifswald.de>
2024-01-11 14:19:59 +0100

    feat: allow use of visit_detail table for episode of care

--- execution_engine/converter/characteristic/episode_of_care.py
+++ execution_engine/converter/characteristic/episode_of_care.py
@@ -1,9 +1,14 @@
+from typing import Type, cast
+
 from execution_engine.constants import LOINC_EPISODE_OF_CARE_TYPE
 from execution_engine.converter.characteristic.codeable_concept import (
     AbstractCodeableConceptCharacteristic,
 )
+from execution_engine.omop.criterion.concept import ConceptCriterion
+from execution_engine.omop.criterion.visit_detail import VisitDetail
 from execution_engine.omop.criterion.visit_occurrence import VisitOccurrence
 from execution_engine.omop.vocabulary import LOINC
+from execution_engine.settings import config
 
 
 class EpisodeOfCareCharacteristic(AbstractCodeableConceptCharacteristic):
@@ -11,5 +16,10 @@ class EpisodeOfCareCharacteristic(AbstractCodeableConceptCharacteristic):
 
     _concept_code = LOINC_EPISODE_OF_CARE_TYPE
     _concept_vocabulary = LOINC
-    _criterion_class = VisitOccurrence
+    _criterion_class = cast(
+        Type[ConceptCriterion],
+        VisitOccurrence
+        if not config.celida_ee_episode_of_care_visit_detail
+        else VisitDetail,
+    )
     _concept_value_static = False
