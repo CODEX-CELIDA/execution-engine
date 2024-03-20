d1e353ed2e438b211f20dce38880defbe3d3944e
Gregor Lichtner <gregor.lichtner@med.uni-greifswald.de>
2024-03-20 15:31:40 +0100

    tests(sepsis): using new test framework

--- tests/recommendation/sepsis-treatment/test_sepsis_m1_tidal_volume.py
+++ tests/recommendation/sepsis-treatment/test_sepsis_m1_tidal_volume.py
@@ -1,22 +1,38 @@
-import pytest
-
 from execution_engine.omop.db.base import (  # noqa: F401 -- do not remove - needed for sqlalchemy to work
     Base,
     metadata,
 )
-from tests.recommendation.test_recommendation_base import TestRecommendationBase
+from tests._testdata.generator.generators import (
+    ARDS,
+    HeightByIdealBodyWeight,
+    TidalVolume,
+    Ventilated,
+)
+from tests.recommendation.test_recommendation_base_v2 import TestRecommendationBaseV2
+
+HeightFor70kgIdealBodyWeight = HeightByIdealBodyWeight(ideal_body_weight=70)
 
 
-@pytest.mark.recommendation
-class TestRecommendationSepsisM1TidalVolume(TestRecommendationBase):
+class TestRecommendationSepsisM1TidalVolume(TestRecommendationBaseV2):
     recommendation_url = "sepsis/recommendation/ventilation-plan-ards-tidal-volume"
     recommendation_package_version = "latest"
+
     recommendation_expression = {
         "Ventilation_Plan_ARDS": {
-            "population": "VENTILATED & ARDS",
-            "intervention": "TIDAL_VOLUME<",
+            "population": Ventilated() & ARDS(),
+            "intervention": TidalVolume(
+                weight=HeightFor70kgIdealBodyWeight.ideal_body_weight
+            )
+            & HeightFor70kgIdealBodyWeight,
         },
     }
+
+    combinations = [
+        Ventilated()
+        | HeightFor70kgIdealBodyWeight
+        | ARDS()
+        | TidalVolume(weight=HeightFor70kgIdealBodyWeight.ideal_body_weight)
+    ]
 
     def test_recommendation_m1_tidal_volume(self, setup_testdata) -> None:
         self.recommendation_test_runner(setup_testdata)
