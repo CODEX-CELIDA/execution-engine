a8f770f2d9d764a1452fb6aa4dd843cccb7eb77b
Jan Moringen <jan.moringen@med.uni-greifswald.de>
2024-12-02 09:08:10 +0100

    fix: include _root attribute in (de)serialization of CriterionCombination
    Before this change, the _root attribute was not included in the
    serialized representation and thus not stored in the database. As a
    result, the execution graph for a recommendation that had been
    restored from the database was different from the execution graph for
    a newly constructed recommendation.

--- execution_engine/omop/criterion/combination/combination.py
+++ execution_engine/omop/criterion/combination/combination.py
@@ -146,6 +146,7 @@ class CriterionCombination(AbstractCriterion, metaclass=ABCMeta):
                 {"class_name": criterion.__class__.__name__, "data": criterion.dict()}
                 for criterion in self._criteria
             ],
+            "root": self._root,
         }
 
     def __invert__(self) -> AbstractCriterion:
@@ -192,6 +193,7 @@ class CriterionCombination(AbstractCriterion, metaclass=ABCMeta):
         combination = cls(
             operator=operator,
             category=category,
+            root_combination=data["root"],
         )
 
         for criterion in data["criteria"]:
